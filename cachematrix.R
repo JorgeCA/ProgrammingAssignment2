5770675d3072f3c0aee8146ca23a7810d0ba0107
Jorge CA <macbluefox@Mac-mini-de-Jorge.local>
2021-01-12 17:26:34 +0100

    writing makeCacheMatrix using the example's steps

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#As in the example, we create a special object that stores a matrix and caches its inverse doing these steps:
+        # 1) set the value of the matrix
+        # 2) get the value of the matrix
+        # 3) set the value of the inverse
+        # 4) get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set,
+             get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
